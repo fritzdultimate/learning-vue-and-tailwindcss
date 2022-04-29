85974ce82b5a14ff42f178d56fab544fbf681442
Darlington <fritzdultimate@gmail.com>
2022-04-29 10:39:05 -0700

    mobile home retouching #1

--- src/modules/home/views/HomeView.vue
+++ src/modules/home/views/HomeView.vue
@@ -6,25 +6,42 @@
             </div>
             <section class="wf-flex wf-flex-wrap wf-justify-center">
                 <div class="wf-flex">
-                    <div class="wf-h-14 wf-w-14 wf-bg-red-900 wf-m-2 wf-brightness-75" style="box-shadow:-7px 4px 14px 5px #300505"></div>
-                    <div class="wf-h-14 wf-w-14 wf-bg-slate-300 wf-m-2 wf-translate-x-2 -wf-translate-y-2" style="box-shadow:-20px 20px 15px 7px #000000"></div>
-                    <div class="wf-h-14 wf-w-14 wf-bg-red-900 wf-m-2" style="box-shadow:-7px 4px 14px 5px #300505"></div>
-                </div>
-                <div class="wf-flex">
-                    <div class="wf-h-14 wf-w-14 wf-bg-red-900 wf-m-2 wf-shadow-red-300" style="box-shadow:-7px 4px 14px 5px #300505"></div>
-                    <div class="wf-h-14 wf-w-14 wf-bg-green-900 wf-m-2" style="box-shadow:-7px 4px 14px 5px #300505"></div>
-                    <div class="wf-h-14 wf-w-14 wf-bg-purple-900 wf-m-2" style="box-shadow:-7px 4px 14px 5px #300505"></div>
-                </div>
-                <div class="wf-flex">
-                    <div class="wf-h-14 wf-w-14 wf-bg-purple-900 wf-m-2" style="box-shadow:-7px 4px 14px 5px #300505"></div>
-                    <div class="wf-h-14 wf-w-14 wf-bg-red-900 wf-m-2 wf-brightness-75" style="box-shadow:-7px 4px 14px 5px #300505"></div>
-                    <div class="wf-h-14 wf-w-14 wf-bg-green-900 wf-m-2" style="box-shadow:-7px 4px 14px 5px #300505">
+                    <div class="wf-flex wf-justify-end wf-items-start wf-h-[97px] wf-w-[100px] wf-relative wf-overflow-hidden wf-bg-transparent before:wf-content-[''] before:wf-bg-black before:wf-h-[50px] before:wf-w-[50px] before:wf-left-[5px] before:wf-top-[8.7px] before:wf-opacity-100 before:wf-z-[1] before:wf-absolute before:wf-rotate-[48deg] after:wf-content-[''] after:wf-bg-black after:wf-h-[90px] after:wf-w-[50px] after:wf-right-[30px] after:wf-bottom-[0.3px] after:wf-opacity-100 after:wf-z-10 after:wf-absolute after:wf-rotate-[48deg] after:wf-origin-bottom-right">
+                        <div class="wf-bg-red-700 wf-h-[70px] wf-w-[70px] wf-relative wf-z-20" style="box-shadow: -30px 27px 0px 0px black;"></div>
+                    </div>
+                    <div class="wf-flex wf-justify-end wf-items-start wf-h-[97px] wf-w-[100px] wf-relative wf-overflow-hidden wf-bg-transparent before:wf-content-[''] before:wf-bg-black before:wf-h-[50px] before:wf-w-[50px] before:wf-left-[5px] before:wf-top-[8.7px] before:wf-opacity-100 before:wf-z-[1] before:wf-absolute before:wf-rotate-[48deg] after:wf-content-[''] after:wf-bg-black after:wf-h-[90px] after:wf-w-[50px] after:wf-right-[30px] after:wf-bottom-[0.3px] after:wf-opacity-100 after:wf-z-10 after:wf-absolute after:wf-rotate-[48deg] after:wf-origin-bottom-right">
+                        <div class="wf-bg-white wf-h-[70px] wf-w-[70px] wf-relative wf-z-20" style="box-shadow: -30px 27px 0px 0px black;"></div>
+                    </div>
+                    <div class="wf-flex wf-justify-end wf-items-start wf-h-[97px] wf-w-[100px] wf-relative wf-overflow-hidden wf-bg-transparent before:wf-content-[''] before:wf-bg-black before:wf-h-[50px] before:wf-w-[50px] before:wf-left-[5px] before:wf-top-[8.7px] before:wf-opacity-100 before:wf-z-[1] before:wf-absolute before:wf-rotate-[48deg] after:wf-content-[''] after:wf-bg-black after:wf-h-[90px] after:wf-w-[50px] after:wf-right-[30px] after:wf-bottom-[0.3px] after:wf-opacity-100 after:wf-z-10 after:wf-absolute after:wf-rotate-[48deg] after:wf-origin-bottom-right">
+                        <div class="wf-bg-red-700 wf-h-[70px] wf-w-[70px] wf-relative wf-z-20" style="box-shadow: -30px 27px 0px 0px black;"></div>
                     </div>
                 </div>
-                <h2 class="wf-font-extrabold wf-text-4xl wf-text-center wf-text-neutral-900 wf-mt-14">
+                <div class="wf-flex">
+                    <div class="wf-flex wf-justify-end wf-items-start wf-h-[97px] wf-w-[100px] wf-relative wf-overflow-hidden wf-bg-transparent before:wf-content-[''] before:wf-bg-black before:wf-h-[50px] before:wf-w-[50px] before:wf-left-[5px] before:wf-top-[8.7px] before:wf-opacity-100 before:wf-z-[1] before:wf-absolute before:wf-rotate-[48deg] after:wf-content-[''] after:wf-bg-black after:wf-h-[90px] after:wf-w-[50px] after:wf-right-[30px] after:wf-bottom-[0.3px] after:wf-opacity-100 after:wf-z-10 after:wf-absolute after:wf-rotate-[48deg] after:wf-origin-bottom-right">
+                        <div class="wf-bg-red-700 wf-h-[70px] wf-w-[70px] wf-relative wf-z-20" style="box-shadow: -30px 27px 0px 0px black;"></div>
+                    </div>
+                    <div class="wf-flex wf-justify-end wf-items-start wf-h-[97px] wf-w-[100px] wf-relative wf-overflow-hidden wf-bg-transparent before:wf-content-[''] before:wf-bg-black before:wf-h-[50px] before:wf-w-[50px] before:wf-left-[5px] before:wf-top-[8.7px] before:wf-opacity-100 before:wf-z-[1] before:wf-absolute before:wf-rotate-[48deg] after:wf-content-[''] after:wf-bg-black after:wf-h-[90px] after:wf-w-[50px] after:wf-right-[30px] after:wf-bottom-[0.3px] after:wf-opacity-100 after:wf-z-10 after:wf-absolute after:wf-rotate-[48deg] after:wf-origin-bottom-right">
+                        <div class="wf-bg-green-900 wf-h-[70px] wf-w-[70px] wf-relative wf-z-20" style="box-shadow: -30px 27px 0px 0px black;"></div>
+                    </div>
+                    <div class="wf-flex wf-justify-end wf-items-start wf-h-[97px] wf-w-[100px] wf-relative wf-overflow-hidden wf-bg-transparent before:wf-content-[''] before:wf-bg-black before:wf-h-[50px] before:wf-w-[50px] before:wf-left-[5px] before:wf-top-[8.7px] before:wf-opacity-100 before:wf-z-[1] before:wf-absolute before:wf-rotate-[48deg] after:wf-content-[''] after:wf-bg-black after:wf-h-[90px] after:wf-w-[50px] after:wf-right-[30px] after:wf-bottom-[0.3px] after:wf-opacity-100 after:wf-z-10 after:wf-absolute after:wf-rotate-[48deg] after:wf-origin-bottom-right">
+                        <div class="wf-bg-purple-900 wf-h-[70px] wf-w-[70px] wf-relative wf-z-20" style="box-shadow: -30px 27px 0px 0px black;"></div>
+                    </div>
+                </div>
+                <div class="wf-flex">
+                    <div class="wf-flex wf-justify-end wf-items-start wf-h-[97px] wf-w-[100px] wf-relative wf-overflow-hidden wf-bg-transparent before:wf-content-[''] before:wf-bg-black before:wf-h-[50px] before:wf-w-[50px] before:wf-left-[5px] before:wf-top-[8.7px] before:wf-opacity-100 before:wf-z-[1] before:wf-absolute before:wf-rotate-[48deg] after:wf-content-[''] after:wf-bg-black after:wf-h-[90px] after:wf-w-[50px] after:wf-right-[30px] after:wf-bottom-[0.3px] after:wf-opacity-100 after:wf-z-10 after:wf-absolute after:wf-rotate-[48deg] after:wf-origin-bottom-right">
+                        <div class="wf-bg-purple-900 wf-h-[70px] wf-w-[70px] wf-relative wf-z-20" style="box-shadow: -30px 27px 0px 0px black;"></div>
+                    </div>
+                    <div class="wf-flex wf-justify-end wf-items-start wf-h-[97px] wf-w-[100px] wf-relative wf-overflow-hidden wf-bg-transparent before:wf-content-[''] before:wf-bg-black before:wf-h-[50px] before:wf-w-[50px] before:wf-left-[5px] before:wf-top-[8.7px] before:wf-opacity-100 before:wf-z-[1] before:wf-absolute before:wf-rotate-[48deg] after:wf-content-[''] after:wf-bg-black after:wf-h-[90px] after:wf-w-[50px] after:wf-right-[30px] after:wf-bottom-[0.3px] after:wf-opacity-100 after:wf-z-10 after:wf-absolute after:wf-rotate-[48deg] after:wf-origin-bottom-right">
+                        <div class="wf-bg-red-700 wf-h-[70px] wf-w-[70px] wf-relative wf-z-20" style="box-shadow: -30px 27px 0px 0px black;"></div>
+                    </div>
+                    <div class="wf-flex wf-justify-end wf-items-start wf-h-[97px] wf-w-[100px] wf-relative wf-overflow-hidden wf-bg-transparent before:wf-content-[''] before:wf-bg-black before:wf-h-[50px] before:wf-w-[50px] before:wf-left-[5px] before:wf-top-[8.7px] before:wf-opacity-100 before:wf-z-[1] before:wf-absolute before:wf-rotate-[48deg] after:wf-content-[''] after:wf-bg-black after:wf-h-[90px] after:wf-w-[50px] after:wf-right-[30px] after:wf-bottom-[0.3px] after:wf-opacity-100 after:wf-z-10 after:wf-absolute after:wf-rotate-[48deg] after:wf-origin-bottom-right">
+                        <div class="wf-bg-green-900 wf-h-[70px] wf-w-[70px] wf-relative wf-z-20" style="box-shadow: -30px 27px 0px 0px black;"></div>
+                    </div>
+                </div>
+                <h2 class="wf-font-extrabold wf-text-4xl wf-text-center wf-text-neutral-900 wf-mt-10">
                     One App That Manage Your Money
                 </h2>
-                <div class="wf-mt-12 wf-flex wf-h-12 wf-w-12 wf-bg-black wf-rounded-full wf-text-white wf-justify-center wf-items-center wf-p-6">
+                <div class="wf-mt-12 wf-flex wf-h-12 wf-w-12 wf-bg-black wf-rounded-full wf-text-white wf-justify-center wf-items-center">
                     <span class="wf-text-xl wf-text-slate-400">></span>
                 </div>
             </section>
