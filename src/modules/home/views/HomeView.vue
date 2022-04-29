4d1bc9477b4ad5b40c8c77ff309be5260fc21cc4
Darlington <fritzdultimate@gmail.com>
2022-04-29 11:02:49 -0700

    heroicon/vue dependency import

--- src/modules/home/views/HomeView.vue
+++ src/modules/home/views/HomeView.vue
@@ -2,14 +2,14 @@
     <main class="wf-block wf-h-full">
         <div class="wf-h-full wf-flex wf-justify-center wf-items-center wf-flex-wrap">
             <div class="wf-flex wf-mt-10 wf-self-end wf-skew-x-12 wf-skew-y-12">
-                <div class="wf-h-4 wf-w-4 wf-bg-white/[0.15] wf-self-end wf-justify-self-end"></div>
+                <div class="wf-h-4 wf-w-4 wf-bg-white/[0.75] wf-self-end wf-justify-self-end"></div>
             </div>
             <section class="wf-flex wf-flex-wrap wf-justify-center">
                 <div class="wf-flex">
                     <div class="wf-flex wf-justify-end wf-items-start wf-h-[97px] wf-w-[100px] wf-relative wf-overflow-hidden wf-bg-transparent before:wf-content-[''] before:wf-bg-black before:wf-h-[50px] before:wf-w-[50px] before:wf-left-[5px] before:wf-top-[8.7px] before:wf-opacity-100 before:wf-z-[1] before:wf-absolute before:wf-rotate-[48deg] after:wf-content-[''] after:wf-bg-black after:wf-h-[90px] after:wf-w-[50px] after:wf-right-[30px] after:wf-bottom-[0.3px] after:wf-opacity-100 after:wf-z-10 after:wf-absolute after:wf-rotate-[48deg] after:wf-origin-bottom-right">
                         <div class="wf-bg-red-700 wf-h-[70px] wf-w-[70px] wf-relative wf-z-20" style="box-shadow: -30px 27px 0px 0px black;"></div>
                     </div>
-                    <div class="wf-flex wf-justify-end wf-items-start wf-h-[97px] wf-w-[100px] wf-relative wf-overflow-hidden wf-bg-transparent before:wf-content-[''] before:wf-bg-black before:wf-h-[50px] before:wf-w-[50px] before:wf-left-[5px] before:wf-top-[8.7px] before:wf-opacity-100 before:wf-z-[1] before:wf-absolute before:wf-rotate-[48deg] after:wf-content-[''] after:wf-bg-black after:wf-h-[90px] after:wf-w-[50px] after:wf-right-[30px] after:wf-bottom-[0.3px] after:wf-opacity-100 after:wf-z-10 after:wf-absolute after:wf-rotate-[48deg] after:wf-origin-bottom-right">
+                    <div class="wf-flex wf-justify-end wf-items-start wf-h-[97px] wf-w-[100px] wf-relative wf-overflow-hidden wf-bg-transparent before:wf-content-[''] before:wf-bg-black before:wf-h-[50px] before:wf-w-[50px] before:wf-left-[5px] before:wf-top-[8.7px] before:wf-opacity-100 before:wf-z-[1] before:wf-absolute before:wf-rotate-[48deg] after:wf-content-[''] after:wf-bg-black after:wf-h-[90px] after:wf-w-[50px] after:wf-right-[30px] after:wf-bottom-[0.3px] after:wf-opacity-100 after:wf-z-10 after:wf-absolute after:wf-rotate-[48deg] after:wf-origin-bottom-right wf-translate-x-3 -wf-translate-y-5 wf-z-50">
                         <div class="wf-bg-white wf-h-[70px] wf-w-[70px] wf-relative wf-z-20" style="box-shadow: -30px 27px 0px 0px black;"></div>
                     </div>
                     <div class="wf-flex wf-justify-end wf-items-start wf-h-[97px] wf-w-[100px] wf-relative wf-overflow-hidden wf-bg-transparent before:wf-content-[''] before:wf-bg-black before:wf-h-[50px] before:wf-w-[50px] before:wf-left-[5px] before:wf-top-[8.7px] before:wf-opacity-100 before:wf-z-[1] before:wf-absolute before:wf-rotate-[48deg] after:wf-content-[''] after:wf-bg-black after:wf-h-[90px] after:wf-w-[50px] after:wf-right-[30px] after:wf-bottom-[0.3px] after:wf-opacity-100 after:wf-z-10 after:wf-absolute after:wf-rotate-[48deg] after:wf-origin-bottom-right">
