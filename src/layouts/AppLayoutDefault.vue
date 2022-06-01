c5403fdfa11d1a2de16f54ec13492341f50a24d4
nobledsmarts <35113848+Nobledsmarts@users.noreply.github.com>
2022-06-01 14:15:21 +0100

    added : sticky nav bar

--- src/layouts/AppLayoutDefault.vue
+++ src/layouts/AppLayoutDefault.vue
@@ -1,5 +1,3 @@
 <template>
-  <div class="wf-h-ful">
     <slot />
-  </div>
 </template>
--- src/layouts/AppLayoutHome.vue
+++ src/layouts/AppLayoutHome.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="wf-bg-gradient-to-r wf-from-gray-900 md:wf-from-gray-900/95 md:wf-via-gray-900 wf-to-gray-900/95">
-        <main class="wf-h-full wf-containerm wf-w-full wf-m-auto" role="main">
+  <!-- <div class=""> -->
+        <main class="wf-bg-gradient-to-r wf-from-gray-900 md:wf-from-gray-900/95 md:wf-via-gray-900 wf-to-gray-900/95 wf-h-full  wf-w-full wf-m-auto" role="main">
             <HeaderComponent  class="md:wf-px-1 md:wf-pt-4"/>
             <slot />
             <FooterComponent  class="md:wf-px-1 md:wf-pt-4"/>
         </main>
-  </div>
+  <!-- </div> -->
 </template>
 
 <script setup lang="ts">
