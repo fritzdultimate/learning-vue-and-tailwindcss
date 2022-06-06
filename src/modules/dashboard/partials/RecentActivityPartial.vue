006afa510782b49372e6a7961a9678556c6f8a1a
Darlington <fritzdultimate@gmail.com>
2022-06-06 21:16:31 +0000

    dashboard

--- src/modules/dashboard/partials/RecentActivityPartial.vue
+++ src/modules/dashboard/partials/RecentActivityPartial.vue
@@ -13,40 +13,22 @@
         </div>
         <!-- Activities -->
         <div class="wf-flex wf-flex-col wf-mt-5">
-            <div class="wf-flex wf-justify-between wf-mb-5">
+            <div class="wf-flex wf-justify-between wf-mb-5" v-for="i in 10" :key="i">
                 <div class="wf-flex">
-                    <figure class="wf-p-1 wf-rounded-full wf-bg-gray-100 wf-shadow wf-flex wf-justify-center wf-items-center wf-mr-5">
-                        <img src="../../../../src/assets/img/fritz_1.jpg" class="wf-cover wf-w-9 wf-h-9 wf-rounded-full">
+                    <figure class="wf-p-px wf-rounded-full wf-bg-gray-100 wf-shadow wf-flex wf-justify-center wf-items-center wf-mr-3">
+                        <img src="../../../../src/assets/img/fritz_1.jpg" class="wf-cover wf-w-7 wf-h-7 wf-rounded-full">
                     </figure>
                     <p class="wf-flex wf-flex-col">
-                        <span class="wf-text-base wf-font-bold wf-text-gray-900">
+                        <span class="wf-text-sm wf-font-bold wf-text-gray-900">
                             Ethereum deposit
                         </span>
-                        <span class="wf-text-sm wf-font-medium wf-text-gray-500">
+                        <span class="wf-text-xs wf-font-medium wf-text-gray-500">
                             08 Jan, 2022
                         </span>
                     </p>
                 </div>
-                <span class="wf-text-base wf-font-bold wf-text-gray-900">
+                <span class="wf-text-sm wf-font-bold wf-text-gray-900">
                     $899.0
-                </span>
-            </div>
-            <div class="wf-flex wf-justify-between wf-mb-5">
-                <div class="wf-flex">
-                    <figure class="wf-p-1 wf-rounded-full wf-bg-gray-100 wf-shadow wf-flex wf-justify-center wf-items-center wf-mr-5">
-                        <img src="../../../../src/assets/img/fritz_1.jpg" class="wf-cover wf-w-9 wf-h-9 wf-rounded-full">
-                    </figure>
-                    <p class="wf-flex wf-flex-col">
-                        <span class="wf-text-base wf-font-bold wf-text-gray-900">
-                            Bitcoin deposit
-                        </span>
-                        <span class="wf-text-sm wf-font-medium wf-text-gray-500">
-                            08 Jan, 2022
-                        </span>
-                    </p>
-                </div>
-                <span class="wf-text-base wf-font-bold wf-text-gray-900">
-                    $299.0
                 </span>
             </div>
         </div>
--- src/modules/dashboard/views/UserDashboardView.vue
+++ src/modules/dashboard/views/UserDashboardView.vue
@@ -141,7 +141,6 @@
                 <div class="wf-h-full wf-bg-white/75 wf-rounded-r-3xl wf-shadow-lg wf-px-3 wf-pb-5">
                     <div class="wf-flex wf-pt-10 wf-items-center">
                         <NotificationsHeaderPartial class="wf-cursor-pointer"/>
-
                         <div class="wf-ml-auto">
                             <figure class="wf-rounded-full wf-flex wf-justify-center wf-items-center wf-cursor-pointer">
                                 <img src="../../../../src/assets/img/fritz_1.jpg" class="wf-cover wf-w-8 wf-h-8 wf-rounded-full">
@@ -151,6 +150,47 @@
                                 </span>
                             </figure>
                         </div>
+                    </div>
+                    <div v-for="(balance, i) in balanceSideBarRight" :key="i" class="wf-relative wf-overflow-hidden wf-mt-5 wf-rounded-lg wf-mx-2 wf-flex wf-flex-col wf-pl-3 wf-py-2 wf-shadow-md" :class="[balance.bgColor, balance.shadowColor]">
+                        <span class="wf-font-bold" :class="[balance.textColor]">{{ balance.name }}</span>
+                        <span class="wf-text-2xl wf-font-bold" :class="[balance.textColor]">
+                            ${{ balance.balance }}
+                        </span>
+
+                        <div class="wf-flex wf-flex-col wf-pt-3 wf-relative wf-z-10">
+                            <h5 class="wf-text-sm wf-font-medium" :class="[balance.textColor]">
+                                Monthly profit
+                            </h5>
+                            <p class="wf-flex">
+                                <span class="wf-text-[18px] wf-font-medium" :class="[balance.textColor]">
+                                    ${{ balance.monthly }}
+                                </span>
+                                <span class="wf-ml-auto wf-mr-3 wf-px-3 wf-rounded-full wf-font-medium wf-text-xs wf-flex wf-justify-center wf-items-center" :class="[balance.gain.textColor, balance.gain.bgColor]">
+                                    +10%
+                                </span>
+                            </p>
+                        </div>
+                        <div class="wf-absolute -wf-top-[50%]  wf-right-0 wf-w-20 wf-h-20 wf-border-[20px] wf-opacity-10 wf-box-content wf-rounded-full"></div>
+                         <div class="wf-absolute -wf-bottom-[50%]  wf-left-0 wf-w-20 wf-h-20 wf-border-[20px] wf-opacity-10 wf-box-content wf-rounded-full"></div>
+                    </div>
+
+                    <div class="wf-mx-2 wf-my-5 wf-flex wf-justify-center">
+                        <div class="wf-w-1/2 wf-flex wf-justify-center">
+                            <button class="wf-mr-3 wf-w-full wf-flex wf-items-center wf-justify-start wf-px-3 wf-py-2 wf-rounded-md wf-bg-purple-700 wf-text-purple-100 hover:wf-bg-purple-900 wf-text-sm">
+                                <IdentificationIcon class="wf-w-6 wf-h-6 wf-mr-1" />
+                                NFTs
+                            </button>
+                        </div>
+                        <div class="wf-w-1/2 wf-flex wf-justify-center">
+                            <button class="wf-mr- wf-w-full wf-flex wf-items-center wf-justify-start wf-px-3 wf-py-2 wf-rounded-md wf-bg-sky-700 wf-text-sky-100 hover:wf-bg-sky-900 wf-text-sm">
+                                <HomeIcon class="wf-w-6 wf-h-6 wf-mr-1" />
+                                Real Estate
+                            </button>
+                        </div>
+                    </div>
+
+                    <div class="wf-flex wf-flex-col wf-mt-10 wf-mx-2">
+                        <RecentActivityPartial class="wf-hidden md:wf-block"/>
                     </div>
                 </div>
             </div>
@@ -162,7 +202,7 @@
 <script setup lang="ts">
     import { Wallet } from 'mdue';
     import { HomeIcon, ChartSquareBarIcon, ViewGridIcon, ReplyIcon, TrendingDownIcon, TrendingUpIcon, ChevronDownIcon } from '@heroicons/vue/solid'
-    import { PlusIcon, SearchIcon, UserIcon, CreditCardIcon, ShoppingCartIcon } from '@heroicons/vue/outline'
+    import { PlusIcon, SearchIcon, UserIcon, CreditCardIcon, ShoppingCartIcon, IdentificationIcon } from '@heroicons/vue/outline'
     import NotificationsHeaderPartial from '../partials/NotificationsHeaderPartial.vue'
     import UserMenuHeaderPartial from '../partials/UserMenuHeaderPartial.vue'
     import CoinDataPartial from '../partials/CoinDataPartial.vue'
@@ -240,4 +280,70 @@
     function navigate(nav) {
         activeLink.value = nav.id;
     }
+
+    function computeBalanceCardBgColor(color, strength) {
+        return `wf-bg-${color}-${strength}`;
+    }
+    const balanceSideBarRight = [
+        {
+            name: 'Total Balance',
+            balance: 50000,
+            monthly: 12.5,
+            bgColor: 'wf-bg-gray-600',
+            shadowColor: 'wf-shadow-gray-700',
+            textColor: 'wf-text-gray-100',
+            gain: {
+                textColor: 'wf-bg-gray-700',
+                bgColor: 'wf-text-gray-200',
+            }
+        },
+        {
+            name: 'Crypto Currency',
+            balance: 50000,
+            monthly: 12.5,
+            bgColor: 'wf-bg-yellow-600',
+            shadowColor: 'wf-shadow-yellow-700',
+            textColor: 'wf-text-yellow-100',
+            gain: {
+                textColor: 'wf-bg-yellow-700',
+                bgColor: 'wf-text-yellow-200',
+            }
+        },
+        {
+            name: 'Live Stock',
+            balance: 60000,
+            monthly: 12.5,
+            bgColor: 'wf-bg-green-600',
+            shadowColor: 'wf-shadow-green-700',
+            textColor: 'wf-text-green-100',
+            gain: {
+                textColor: 'wf-bg-green-700',
+                bgColor: 'wf-text-green-200',
+            }
+        },
+        {
+            name: 'Real Estate',
+            balance: 50000,
+            monthly: 12.5,
+            bgColor: 'wf-bg-red-600',
+            shadowColor: 'wf-shadow-red-700',
+            textColor: 'wf-text-red-100',
+            gain: {
+                textColor: 'wf-bg-red-700',
+                bgColor: 'wf-text-red-200',
+            }
+        },
+        {
+            name: 'NFTs',
+            balance: 560000,
+            monthly: 12.5,
+            bgColor: 'wf-bg-blue-600',
+            shadowColor: 'wf-shadow-blue-700',
+            textColor: 'wf-text-blue-100',
+            gain: {
+                textColor: 'wf-bg-blue-700',
+                bgColor: 'wf-text-blue-200',
+            }
+        }
+    ]
 </script>
