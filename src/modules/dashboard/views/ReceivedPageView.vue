cd50e00e11bc6a2fed53dcf0d8c36c622e8915d8
Darlington <fritzdultimate@gmail.com>
2022-06-13 01:48:08 +0000

    installed qr code generator

---
+++ src/modules/dashboard/views/ReceivedPageView.vue
@@ -0,0 +1,16 @@
+<template>
+    <ViewWrapper>
+        <div class="wf-flex wf-flex-col">
+            <div class="wf-flex wf-flex-col wf-items-center">
+                <h4 class="wf-text-gray-400 wf-text-base wf-font-bold wf-mb-5">BTC QR Code</h4>
+                <QRCode />
+                <h5 class="wf-text-base wf-text-gray-400 wf-font-bold wf-mb-3">BTC Address</h5>
+            </div>
+        </div>
+    </ViewWrapper>
+</template>
+
+<script setup lang="ts">
+    import ViewWrapper from '../partials/ViewWrapper.vue';
+    import QRCode from '../../../common/components/Cards/QRCode.vue';
+</script>
