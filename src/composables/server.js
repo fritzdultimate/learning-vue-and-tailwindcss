ccb44adf5af3e37170fac18a060731c5b1bb6dcd
nobledsmarts <35113848+Nobledsmarts@users.noreply.github.com>
2022-06-27 20:51:06 +0100

    added : cloud folder plus cloud.js for cloud code

--- src/composables/server.js
+++ src/composables/server.js
@@ -3,3 +3,10 @@ import { connect } from '../services/moralis/connection.js'
 export function useServer(serverUrl, appId) {
     connect(import.meta.env.VITE_SERVER_URL, import.meta.env.VITE_APP_ID);
 }
+
+moralis-admin-cli watch-cloud-folder
+--moralisApiKey L5miPKEuTafJ4St
+--moralisApiSecret SRzXYiBxU8qncvf
+--moralisSubdomain zqqlc9enecak.usemoralis.com
+--autoSave 1
+--moralisCloudfolder C:\Users\HP\OneDrive\Documents\moralis\cloud\spacearn
